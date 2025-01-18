417cee484de3b8e2a2930fd0f56b96c52343fd9c
Christopher <chriuser.email christopher.a31@hotmail.comgit config --global user.name Chris>
2025-01-18 15:20:41 -0500

    Feat: crear conexion a DB

---
+++ src/app.js
@@ -0,0 +1,19 @@
+const express = require("express");
+const bodyParser = require("body-parser");
+const connectDB = require("./config/dbConfig");
+require("dotenv").config();
+
+const app = express();
+connectDB();
+
+// Middlewares
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+
+// Rutas
+app.use("/api/bot", require("./routes/botRoutes"));
+app.use("/api/cart", require("./routes/cartRoutes"));
+
+// Servidor
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, () => console.log(`Servidor corriendo en el puerto ${PORT}`));
